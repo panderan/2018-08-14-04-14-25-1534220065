eef96477125297c79fcfbacd8bce86516d1c71d0
Pan Deran <panderan@163.com>
2019-05-22 18:34:19 +0800

    passed test

--- main/main.js
+++ main/main.js
@@ -1,4 +1,20 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(numstr) {
+    numstr_templates_top = ["._.","...","._.","._.","...","._.","._.","._.","._.","._."]
+    numstr_templates_mid = ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"]
+    numstr_templates_bom = ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]
+    
+    line_top = ""
+    line_mid = ""
+    line_bom = ""
+    for (var i=0; i < numstr.length; i++) {
+        idx = parseInt(numstr[i])
+        line_top += numstr_templates_top[idx] + " "
+        line_mid += numstr_templates_mid[idx] + " "
+        line_bom += numstr_templates_bom[idx] + " "
+    }
+    
+    line_top = line_top.trim()
+    line_mid = line_mid.trim()
+    line_bom = line_bom.trim()    
+    return line_top+"\n"+line_mid+"\n"+line_bom+"\n"
 };
